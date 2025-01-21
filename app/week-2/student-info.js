b59589ba39716223689d178a4790f3413cc4f817
DevainSikka <devain97@gmail.com>
2025-01-21 10:53:48 -0700

    w2 done

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+export default function(){
+    return (
+        <div>
+ <h1>Devain</h1>
+        <Link href="https://github.com/DevainSikka">https://github.com/DevainSikka</Link>
+        </div>
+       
+    )
+}
